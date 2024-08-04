1f7f662b2decb34a5160b1bcc4d68c18ba029be4
KSPSSW <kissapongs@gmail.com>
2024-08-04 18:50:23 +0700

    Start

---
+++ src/main/java/com/mycompany/oxgui/Oxgui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oxgui;
+
+/**
+ *
+ * @author kissa
+ */
+public class Oxgui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
